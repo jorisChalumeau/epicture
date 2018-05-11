63c4a65bf01d4e62f5deee274449fa387ae61eb2
TheApac <apac.alexcuevas@gmail.com>
2018-05-12 00:36:49 +0200

    Upload image visible and clearable
    Image is visible at select and invisible when clear is clicked

--- app/src/main/java/eu/epitech/spartan/epicture/activities/UploadActivity.java
+++ app/src/main/java/eu/epitech/spartan/epicture/activities/UploadActivity.java
@@ -1,14 +1,23 @@
 package eu.epitech.spartan.epicture.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 import eu.epitech.spartan.epicture.R;
 
 public class UploadActivity extends AppCompatActivity {
+
+    private static final int SELECT_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +50,10 @@ public class UploadActivity extends AppCompatActivity {
 
     private void choosePictureToUpload() {
         // TODO : browse through storage pictures
-
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        photoPickerIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
         // ------------ on select -------------
         // enable upload and clear buttons
         Button bUpload = findViewById(R.id.uploadButton);
@@ -65,11 +77,27 @@ public class UploadActivity extends AppCompatActivity {
      */
     private void clearPictures() {
         // TODO : remove pictures
-
+        ImageView imageView = findViewById(R.id.toUploadImgView);
+        imageView.setImageBitmap(null);
         // disable upload and clear buttons
         Button bUpload = findViewById(R.id.uploadButton);
         Button bClear = findViewById(R.id.clearButton);
         bUpload.setEnabled(false);
         bClear.setEnabled(false);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            try {
+                Bitmap image = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                ImageView imageView = findViewById(R.id.toUploadImgView);
+                imageView.setImageBitmap(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
